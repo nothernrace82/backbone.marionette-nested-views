a6f20e1731494b1d8ef13dfd16b46a5d666544c3
SpringBoy <springboy@SpringBoys-Mac.local>
2015-09-12 22:38:11 +0200

    first step on collectionView

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,1 +1,19 @@
-// nothing to see yet !
+MyApp = new Backbone.Marionette.Application();
+
+MyApp.addRegions({
+  mainRegion: "#content"
+});
+
+Hero = Backbone.Model.extend({});
+
+Heroes = Backbone.Collection.extend({
+  model: Hero
+});
+
+HeroView = Backbone.Marionette.ItemView.extend({
+  template: "#accordion-group-template"
+});
+
+AccordionView = Backbone.Marionette.CollectionView.extend({
+  itemView: HeroView
+});
